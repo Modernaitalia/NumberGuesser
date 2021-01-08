23a4fcc0b59e055bed562ca24d3d4372a5670f3e
Bruce Watkins II <dagoose@bruces-mbp.lan>
2021-01-07 19:01:25 -0500

    changing logic to allow for retrying after guessing wrong

--- generatorScript.py
+++ generatorScript.py
@@ -1,14 +1,23 @@
 import random
 import logging
 
-randomNumber = random.seed(a=5)
+randomNumber = random.randint(1,10)
+print(randomNumber)
 logging.info("Random number generated")
 
 print("User try to guess the random number that was created")
-userGuess = input("PLease enter a number:\n")
-if userGuess > randomNumber:
-    print("Your guess is too high, try again!")
-elif userGuess < randomNumber:
-    print("Your guess is too low, try again!")
-else:
-    print("Your guess was spot on. Congradulations, You win!")
+userGuess = input("Please enter a number:\n")
+x = 0
+while int(userGuess) != randomNumber:
+    if int(userGuess) > randomNumber:
+        print("Your guess is too high, try again!")
+        continue
+        x+=1
+    if int(userGuess) < randomNumber:
+        print("Your guess is too low, try again!")
+        continue
+        x+=1
+
+    if int(userGuess) == randomNumber:
+        print("Your guess was spot on. Congratulations, You win!")
+        break
